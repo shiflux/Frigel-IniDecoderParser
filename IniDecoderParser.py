ac3f7f2a8d9086b972e8e6f626f13836fdfece95
M. Hassan Khurshid <h.khurshid@frigel.com>
2017-01-31 14:10:55 +0100

    Add minor fixes

--- IniDecoderParser.py
+++ IniDecoderParser.py
@@ -1,8 +1,9 @@
-### decoder = IniDecoderParser("~DIR~/FILEINI.ini", 1)
-### decoder.ini_dict
+# decoder = IniDecoderParser("~DIR~/FILEINI.ini", 1)
+# decoder.ini_dict
 
 
 class IniDecoderParser:
+
     def __init__(self, ini_file, family):
         self.ini_dict = dict()
         self.create_dict()
@@ -20,56 +21,85 @@ class IniDecoderParser:
             splitted = line.split('=', 1)[1].split(',')
             code = "1:anin:" + splitted[0].strip()
             name = splitted[2].strip()
-            unit = splitted[2].strip().strip('_')
-            self.ini_dict["Analog"].append({"CODE": code, "FAMILY_ID": self.family, "UNIT": unit, "NAME": name})
+            unit = splitted[3].strip().strip('_')
+            self.ini_dict["analog"].append({
+                "code": code, "family_id": self.family, "unit": unit,
+                "name": name, "type": "anin"
+            })
         elif line.startswith("AO"):
             splitted = line.split('=', 1)[1].split(',')
             code = "1:anout:" + splitted[0].strip()
             name = splitted[2].strip()
-            unit = splitted[2].strip().strip('_')
-            self.ini_dict["Analog"].append({"CODE": code, "FAMILY_ID": self.family, "UNIT": unit, "NAME": name})
+            unit = splitted[3].strip().strip('_')
+            self.ini_dict["analog"].append({
+                "code": code, "family_id": self.family, "unit": unit,
+                "name": name, "type": "anout"
+            })
+
         elif line.startswith("CMD"):
             splitted = line.split('=', 1)[1].split(';')
             code = "1:command:" + splitted[0].strip()
             name = splitted[1].strip()
-            self.ini_dict["Command"].append({"CODE": code, "FAMILY_ID": self.family, "NAME": name})
+            self.ini_dict["command"].append({
+                "code": code, "family_id": self.family, "name": name
+            })
+
         elif line.startswith("EVN"):
             splitted = line.split('=', 1)[1].split(',')
             code = "1:alarm:" + splitted[0].strip()
             name = splitted[1].strip()
-            self.ini_dict["Digital"].append({"CODE": code, "FAMILY_ID": self.family, "NAME": name})
+            self.ini_dict["digital"].append({
+                "code": code, "family_id": self.family, "name": name,
+                "type": "alarm"
+            })
+
         elif line.startswith("SETUP"):
             splitted = line.split('=', 1)[1].split(';')
             temp_s = splitted[0].strip()
             code = "1:setup:" + temp_s[temp_s.find("(") + 1:temp_s.find(")")]
             name = splitted[0].strip()
             unit = splitted[6].strip().strip('_')
-            local_min = splitted[8].strip()
-            local_max = splitted[7].strip()
+            min_value = float(splitted[8].strip())
+            max_value = float(splitted[7].strip())
             if splitted[0].endswith(')'):
                 possibile_values = splitted[0].split('(')[2].split(')')[0]
             else:
                 possibile_values = ''
-            self.ini_dict["Setup"].append({"CODE": code, "FAMILY_ID": self.family, "UNIT": unit, "NAME": name,
-                                          "MIN": local_min, "MAX": local_max, "POSSIBLE_VALUES": possibile_values})
+            self.ini_dict["setup"].append({
+                "code": code, "family_id": self.family, "unit": unit,
+                "name": name, "min_value": min_value, "max_value": max_value,
+                "possible_values": possibile_values
+            })
         elif line.startswith("DI"):
             splitted = line.split('=', 1)[1].split(',')
             code = "1:digin:" + splitted[0].strip()
             name = splitted[1].strip()
-            self.ini_dict["Digital"].append({"CODE": code, "FAMILY_ID": self.family, "NAME": name})
+            self.ini_dict["digital"].append({
+                "code": code, "family_id": self.family, "name": name,
+                "type": "digin"
+            })
+
         elif line.startswith("DO"):
             splitted = line.split('=', 1)[1].split(',')
             code = "1:digout:" + splitted[0].strip()
             name = splitted[1].strip()
-            self.ini_dict["Digital"].append({"CODE": code, "FAMILY_ID": self.family, "NAME": name})
+            self.ini_dict["digital"].append({
+                "code": code, "family_id": self.family, "name": name,
+                "type": "digout"
+
+            })
+
         elif line.startswith("STS"):
             splitted = line.split('=', 1)[1].split(',')
             code = "1:status:" + splitted[1].strip()
             name = splitted[2].strip()
-            self.ini_dict["Digital"].append({"CODE": code, "FAMILY_ID": self.family, "NAME": name})
+            self.ini_dict["digital"].append({
+                "code": code, "family_id": self.family, "name": name,
+                "type": "status"
+            })
 
     def create_dict(self):
-        self.ini_dict["Analog"] = []
-        self.ini_dict["Digital"] = []
-        self.ini_dict["Command"] = []
-        self.ini_dict["Setup"] = []
+        self.ini_dict["analog"] = []
+        self.ini_dict["digital"] = []
+        self.ini_dict["command"] = []
+        self.ini_dict["setup"] = []
